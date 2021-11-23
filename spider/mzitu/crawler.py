5f18d3317f8a75c7d4eef48ce9636eb00737a5ea
youyouzh <1406558940@qq.com>
2021-11-23 19:13:26 +0800

    add mzitu picture crawler.

---
+++ spider/mzitu/crawler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import shutil
+from bs4 import BeautifulSoup
+
+import u_base.u_log as log
+import u_base.u_file as u_file
+
+"""
+爬取妹子图网图片
+https://www.mzitu.com/
+"""
+
+
+if __name__ == '__main__':
