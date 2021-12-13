eeb7c129de94152e7c945f1105eeb4ea96b9af31
youyouzh <1406558940@qq.com>
2021-12-14 00:52:38 +0800

    add word_book table and model.

--- spider/word/database/db.py
+++ spider/word/database/db.py
@@ -5,8 +5,6 @@ from sqlalchemy import Column, Date, Index, String, TIMESTAMP, Text, text
 from sqlalchemy.dialects.mysql import BIGINT, INTEGER, TINYINT
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from u_base import u_log
-from u_base import u_file
 
 BaseModel = declarative_base()
 metadata = BaseModel.metadata
@@ -32,6 +30,26 @@ class Grammar(BaseModel):
     updated_at = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
 
 
+class WordBook(BaseModel):
+    __tablename__ = 'word_book'
+
+    id = Column(BIGINT(20), primary_key=True)
+    name = Column(String(128), nullable=False, server_default=text("''"))
+    source = Column(String(64), nullable=False, server_default=text("''"))
+    source_id = Column(BIGINT(20), primary_key=True)
+    level = Column(String(16), nullable=False, server_default=text("''"))
+    introduction = Column(String(256), nullable=False, server_default=text("''"))
+    cover_image_url = Column(String(256), nullable=False, server_default=text("''"))
+    from_lang = Column(String(16), nullable=False, server_default=text("''"))
+    to_lang = Column(String(16), nullable=False, server_default=text("''"))
+    word_count = Column(INTEGER(11), nullable=False, server_default=text("0"))
+    learning_user_count = Column(INTEGER(11), nullable=False, server_default=text("0"))
+    finish_user_count = Column(INTEGER(11), nullable=False, server_default=text("0"))
+    word_queried = Column(TINYINT(1), nullable=False, server_default=text("0"))
+    created_at = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    updated_at = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+
+
 def save_grammar(grammar: Grammar):
     if session.query(Grammar).filter(Grammar.id == grammar.id).first() is not None:
         print("The Grammar is exist. id:{} , content:{}".format(grammar.id, grammar.content))
@@ -39,3 +57,7 @@ def save_grammar(grammar: Grammar):
 
     session.merge(grammar)
     session.commit()
+
+
+def ready_table():
+    BaseModel.metadata.create_all(engine)
--- spider/word/database/table.sql
+++ spider/word/database/table.sql
@@ -6,7 +6,7 @@ drop table if exists grammar;
 
 create table grammar
 (
-	id bigint auto_increment,
+	id bigint auto_increment primary key,
 	content varchar(256) not null comment '语法内容',
 	level varchar(16) not null comment '语法等级',
 	category varchar(64) not null comment '分类',
@@ -17,16 +17,52 @@ create table grammar
 	postscript varchar(512) comment '补充说明',
 	created_at timestamp default current_timestamp not null,
 	updated_at timestamp default current_timestamp not null,
-	constraint table_name_pk primary key (id)
+	index `content` (`content`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 comment '日语语法';
 
+create table word_book
+(
+	id bigint auto_increment primary key,
+	source varchar(64) not null comment '词书来源，如沪江，芥末日语考级',
+	source_id bigint not null comment '词汇来源id',
+	name varchar(128) not null comment '词书名称',
+	level varchar(16) not null comment '词书等级，如N1，N2',
+	introduction varchar(256) not null comment '语法接续',
+	cover_image_url varchar(256) not null comment '',
+	from_lang varchar(16) not null comment '词书原语言',
+	to_lang varchar(16) not null comment '词书目标语言',
+	word_count int not null comment '单词数量',
+	learning_user_count int not null comment '学习人数，基于source数据',
+	finish_user_count int not null comment '学完人数，基于source数据',
+	word_queried tinyint(1) not null comment '是否词汇下载完成',
+	created_at timestamp default current_timestamp not null,
+	updated_at timestamp default current_timestamp not null,
+	index `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 comment '词书';
+
+create table word_book_to_word
+(
+    id bigint auto_increment primary key,
+    word_id bigint not null comment '单词id',
+    word_book_id bigint not null comment '词书id',
+	created_at timestamp default current_timestamp not null,
+	updated_at timestamp default current_timestamp not null,
+	index `index_word_book_id_word_id` (`word_book_id`, `word_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 comment '词书对单词映射';
+
 create table word (
-    id bigint auto_increment,
+    id bigint auto_increment primary key,
+    source varchar(64) not null comment '词汇来源，如沪江，芥末日语考级',
+    source_id bigint not null comment '词汇来源id',
     name varchar(128) not null comment '单词',
-    `language` varchar(16) not null comment '语言： jp, zh, eng',
+	from_lang varchar(16) not null comment '词书原语言： jp, zh, en',
+	to_lang varchar(16) not null comment '词书目标语言： jp, zh, en',
     kana varchar(64) not null comment '',
     roma varchar(64) not null comment '',
     accent varchar(64) not null comment '音标',
     level varchar(8) comment '等级',
-    pos varchar(64) not null comment '词性'
-)
+    pos varchar(64) not null comment '词性',
+	created_at timestamp default current_timestamp not null,
+	updated_at timestamp default current_timestamp not null,
+	index `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 comment '单词';
