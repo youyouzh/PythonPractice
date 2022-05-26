96157564e362e853a263f8c8ef6d40b79ca2f064
youyouzh <1406558940@qq.com>
2022-05-26 21:13:18 +0800

    add min_id param.

--- spider/pixiv/mysql/db.py
+++ spider/pixiv/mysql/db.py
@@ -116,13 +116,13 @@ def base_illustration_image(illustration: Illustration) -> dict:
     }
 
 
-def query_top_total_bookmarks(count=100000) -> list:
-    cache_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), r"cache\top_total_bookmarks-2021.json")
+def query_top_total_bookmarks(count=100000, min_id=91391283) -> list:
+    cache_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), r"cache\top_total_bookmarks-2022.json")
     if os.path.isfile(cache_file):
         return json.load(open(cache_file, encoding='utf-8'))
     results = session.query(Illustration.id, Illustration.total_bookmarks, Illustration.total_view)\
         .filter(Illustration.type == 'illust')\
-        .filter(Illustration.id >= 89800864)\
+        .filter(Illustration.id >= min_id)\
         .order_by(Illustration.total_bookmarks.desc()).limit(count).all()
     result = [dict(zip(v.keys(), v)) for v in results]
     json.dump(result, open(cache_file, 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
