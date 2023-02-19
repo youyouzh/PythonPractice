fba67598520e1f3aeec65bd8faa6ae53b51132ea
youyouzh <1406558940@qq.com>
2023-02-19 22:13:22 +0800

    Create serevices-install.py

---
+++ tool/serevices-install.py
@@ -0,0 +1,38 @@
+"""
+pywin32(Python for Windows扩展) windows服务注册
+以管理员身份运行命令提示符,输入:
+
+sc create TestService binpath ="C:\ Python36\Python.exe c:\ PythonFiles\AppServerSvc.py"DisplayName ="TestService"start = auto
+
+"""
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+
+
+class AppServerSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "TestService"
+    _svc_display_name_ = "Test Service"
+
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_,''))
+        self.main()
+
+    def main(self):
+        pass
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(AppServerSvc)
