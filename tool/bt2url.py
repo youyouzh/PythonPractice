54d4d13684f749b41a37a4409072539bd05f385b
uusama <1406558940@qq.com>
2023-02-24 10:13:05 +0800

    format.

--- tool/bt2url.py
+++ tool/bt2url.py
@@ -1,20 +1,18 @@
 #! /usr/local/bin/python 
-# @desc python通过BT种子生成磁力链接  
-# @date 2015/11/10 
-# @author pythontab.com 
+# python通过BT种子生成磁力链接
 import bencode 
 import sys 
 import hashlib 
 import base64 
-import urllib 
-#获取参数 
+
+# 获取参数
 torrentName = sys.argv[1] 
-#读取种子文件 
+# 读取种子文件
 torrent = open(torrentName, 'rb').read() 
-#计算meta数据 
+# 计算meta数据
 metadata = bencode.bdecode(torrent) 
-hashcontents = bencode.bencode(metadata['info']) 
-digest = hashlib.sha1(hashcontents).digest() 
+hash_contents = bencode.bencode(metadata['info'])
+digest = hashlib.sha1(hash_contents).digest()
 b32hash = base64.b32encode(digest) 
-#打印 
-print 'magnet:?xt=urn:btih:%s' % b32hash
+# 打印
+print('magnet:?xt=urn:btih:%s' % b32hash)
