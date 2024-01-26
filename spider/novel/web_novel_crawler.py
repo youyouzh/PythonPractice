2ef7f407cfa5c25f785e1cb70b9d760c216f2e70
uusama <1406558940@qq.com>
2024-01-26 23:35:16 +0800

    refactor with crawl config.

--- spider/novel/web_novel_crawler.py
+++ spider/novel/web_novel_crawler.py
@@ -2,17 +2,15 @@
 小说爬虫
 """
 import json
-import os.path
 import re
 
-import requests
-from urllib.parse import quote
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
 import u_base.u_file as u_file
 import u_base.u_log as log
 
-_REQUESTS_KWARGS = {
+REQUESTS_KWARGS = {
     # 'proxies': {
     #   'socks': 'http://127.0.0.1:1080',  # use proxy
     # },
@@ -35,146 +33,87 @@ _REQUESTS_KWARGS = {
 }
 
 
-SELECTOR_MAP = {
-    'b.faloo.com': {
-        'home_url': 'https://b.faloo.com',
-        'chapter_url': 'div.c_con_list > div.c_con_li_detail_p > a',
-        'chapter_content': 'div.noveContent > p'
-    },
-    'book.sfacg.com': {
-        'home_url': 'https://book.sfacg.com/',
-        'chapter_url': 'div.catalog-list > ul > li > a',
-        'chapter_content': 'div.article-content > p'
-    },
-    'www.69shuba.com': {
-        'home_url': 'https://www.69shuba.com/',
-        'chapter_url': 'div.catalog > ul > li > a',
-        'chapter_content': 'div.txtnav > p'
-    }
-}
+class CrawlConfig(object):
+
+    def __init__(self,
+                 home_url: str,
+                 chapter_url_selector: str = '',
+                 chapter_title_selector: str = '',
+                 chapter_content_selector: str = '',
+                 next_chapter_url_selector: str = '',
+                 ):
+        self.home_url = home_url
+        self.domain = urlparse(self.home_url).netloc
+        self.chapter_url_selector = chapter_url_selector
+        self.chapter_title_selector = chapter_title_selector
+        self.chapter_content_selector = chapter_content_selector
+        self.next_chapter_url_selector = next_chapter_url_selector
 
 
-def patch_novel_info(novel_info: dict) -> dict:
-    book_name = novel_info['bookName']
-    if novel_info['collectCount']:
-        log.info('this book collect info is not need to patch: {}'.format(book_name))
-        # return novel_info
-    if novel_info['crawlSourceName'] == 'SF轻小说':
-        # 首先爬取小说url
-        novel_info_index_url = 'https://index.tsyuri.com/book/{}.html'.format(novel_info['id'])
-        html_content = u_file.get_content_with_cache(novel_info_index_url, **_REQUESTS_KWARGS)
-        soup = BeautifulSoup(html_content, 'lxml')
-
-        novel_index_node = soup.select_one('div.layui-col-xs8 > div > a')
-        if not novel_index_node:
-            log.error('can not extract novel index url: {}'.format(book_name))
-            return novel_info
-        novel_index_url = novel_index_node['href']
-        novel_info['novel_index_url'] = novel_index_url
-        log.info('extract novel index url success. book: {}, url: {}'.format(book_name, novel_index_url))
-
-        html_content = u_file.get_content_with_cache(novel_index_url, **_REQUESTS_KWARGS)
-        soup = BeautifulSoup(html_content, 'lxml')
-
-        book_grade_node = soup.select_one('div#BasicOperation')
-        if not book_grade_node:
-            log.error('basic grade node is empty. book: {}, url: {}'.format(book_name, novel_index_url))
-            return novel_info
-        basic_grade_text = book_grade_node.text.replace('\n', '')
-        log.info('extract book grade info success. book: {}, text: {}'.format(book_name, basic_grade_text))
-        novel_info['likeCount'] = re.compile(r'赞 (\d+)').search(basic_grade_text).groups()[0]
-        novel_info['collectCount'] = re.compile(r'收藏 (\d+)').search(basic_grade_text).groups()[0]
-        return novel_info
-    if novel_info['crawlSourceName'] == '刺猬猫':
-        # 搜索小说，然后匹配获取小说页
-        search_url = 'https://www.ciweimao.com/get-search-book-list/0-0-0-0-0-0/{}/{}/1'\
-            .format(quote('全部'), quote(book_name))
-        html_content = u_file.get_content_with_cache(search_url, **_REQUESTS_KWARGS)
-        soup = BeautifulSoup(html_content, 'lxml')
-
-        search_book_nodes = soup.select('div.cnt > p.tit > a')
-        if not search_book_nodes:
-            log.error('search book result is empty: {}'.format(book_name))
-            return novel_info
-        for search_book_node in search_book_nodes:
-            if search_book_node['title'] == book_name:
-                novel_info['novel_index_url'] = search_book_node['href']
-                break
-        if 'novel_index_url' not in novel_info or not novel_info['novel_index_url']:
-            log.error('search book index url is empty: {}'.format(book_name))
-            return novel_info
-
-        html_content = u_file.get_content_with_cache(novel_info['novel_index_url'], **_REQUESTS_KWARGS)
-        soup = BeautifulSoup(html_content, 'lxml')
-
-        book_grade_node = soup.select_one('p.book-grade')
-        if not book_grade_node:
-            log.error('basic grade node is empty. book: {}, url: {}'
-                      .format(book_name, novel_info['novel_index_url']))
-            return novel_info
-        basic_grade_text = book_grade_node.text.replace('\n', '')
-        log.info('extract book grade info success. book: {}, text: {}'.format(book_name, basic_grade_text))
-        novel_info['likeCount'] = re.compile(r'总点击：([,\d.万]+)').search(basic_grade_text).groups()[0]
-        novel_info['collectCount'] = re.compile(r'总收藏：(\d+)').search(basic_grade_text).groups()[0]
-        return novel_info
+CRAWL_CONFIGS = [
+    CrawlConfig(home_url='https://b.faloo.com',
+                chapter_url_selector='div.c_con_list > div.c_con_li_detail_p > a',
+                chapter_content_selector='div.noveContent > p'
+                ),
+    CrawlConfig(home_url='https://book.sfacg.com/',
+                chapter_url_selector='div.catalog-list > ul > li > a',
+                chapter_content_selector='div.article-content > p'
+                ),
+    CrawlConfig(home_url='https://www.69shuba.com/',
+                chapter_url_selector='div.catalog > ul > li > a',
+                chapter_content_selector='div.txtnav > p',
+                ),
+    CrawlConfig(home_url='https://325def1d9a6.12bqg.com/',
+                chapter_title_selector='span.title',
+                chapter_content_selector='div#chaptercontent',
+                next_chapter_url_selector='a#pb_next'
+                ),
+    CrawlConfig(home_url='https://ncode.syosetu.com/',
+                chapter_title_selector='p.novel_subtitle',
+                chapter_content_selector='div#novel_honbun',
+                next_chapter_url_selector='a.novelview_pager-next'
+                ),
+]
 
 
-def get_novel_list_from_bb(cache_file=''):
-    if cache_file and os.path.isfile(cache_file):
-        return u_file.load_json_from_file(cache_file)
-    query_api = 'https://index.tsyuri.com/book/searchByPage'
-    params = {
-        'curr': 1,
-        'limit': 100,
-        'bookStatus': '1',
-        'wordCountMin': 150000,
-        'wordCountMax': '',
-        'sort': 'click_purity_score',
-        'updatePeriod': '',
-        'purity': '1',
-        'keyword': '',
-        # 'tag': '%2C百合',
-        'tag': '%2C变百%2C百合',
-        'source': '%2CSF轻小说%2C次元姬%2C刺猬猫%2C起点'
-    }
-    response = requests.get(query_api, params=params)
-    if response.status_code != 200:
-        log.error('request query api status code not 200.')
-        return []
-    data = json.loads(response.text)
-    if 'code' not in data or data['code'] != '200' or 'data' not in data:
-        log.error('response data.code is not 200')
-        return []
-    data = data['data']
-    log.info('page_number: {}, size: {}, total: {}'.format(data['pageNum'], data['pageSize'], data['total']))
-    return list(map(lambda x: {
-        'id': x['id'],
-        'catName': x['catName'],
-        'picUrl': x['picUrl'],
-        'bookName': x['bookName'],
-        'newTag': x['newTag'],
-        'authorName': x['authorName'],
-        'bookDesc': x['bookDesc'],
-        'purity': x['purity'],
-        'visitCount': x['visitCount'],
-        'likeCount': 0,
-        'collectCount': 0,
-        'crawlSourceName': x['crawlSourceName'],
-        'tag': x['tag'],
-    }, data['list']))
+def get_crawl_config(source_url: str) -> CrawlConfig:
+    for crawl_config in CRAWL_CONFIGS:
+        if crawl_config.domain == urlparse(source_url).netloc:
+            return crawl_config
+    log.error('can not find the crawl config for url: {}'.format(source_url))
+    raise Exception('can not find crawl config')
 
 
-def crawl_chapter_urls(chapter_index_url: str, selector_tag: str) -> list:
+def patch_url(source_url: str, extract_url: str) -> str:
+    """
+    补全URL，很多页面元素中提取的URL可能不包含host等，进行统一处理
+    :param source_url: 当前页面的URL
+    :param extract_url: 从当前页面元素中提取的URL
+    :return: 补全的URL
+    """
+    if extract_url.startswith('http'):
+        # 包含http开头不需要处理
+        return extract_url
+    source_url_info = urlparse(source_url)
+    if extract_url.startswith('//'):
+        # 需要补全http部分
+        return source_url_info.scheme + extract_url
+    else:
+        # 需要补全域名部分
+        return source_url_info.scheme + '://' + source_url_info.netloc + extract_url
+
+
+def crawl_chapter_urls(chapter_index_url: str, crawl_config: CrawlConfig) -> list:
     """
     爬取章节目录
     :param chapter_index_url: 章节目录页面地址
-    :param selector_tag: 选择器
+    :param crawl_config: 爬取配置
     :return: 章节列表
     """
-    html_content = u_file.get_content_with_cache(chapter_index_url, **_REQUESTS_KWARGS)
+    html_content = u_file.get_content_with_cache(chapter_index_url, **REQUESTS_KWARGS)
     soup = BeautifulSoup(html_content, 'lxml')
 
-    chapter_url_nodes = soup.select(SELECTOR_MAP[selector_tag]['chapter_url'])
+    chapter_url_nodes = soup.select(crawl_config.chapter_url_selector)
     log.info('crawl chapter html content finish. chapter url sizes: {}'.format(len(chapter_url_nodes)))
 
     chapter_urls = []
@@ -182,18 +121,9 @@ def crawl_chapter_urls(chapter_index_url: str, selector_tag: str) -> list:
         if not chapter_url_node or '#' == chapter_url_node['href']:
             log.warn('skip empty chapter url: {}'.format(chapter_url_node))
             continue
-        full_url = chapter_url_node['href']
-        if full_url.startswith('http'):
-            pass
-        elif full_url.startswith('//'):
-            # 处理缩写链接
-            full_url = 'https:' + full_url
-        else:
-            # 处理以缩写域名
-            full_url = 'https://' + selector_tag + full_url
         chapter_index_url = {
             'title': chapter_url_node['title'],
-            'url': full_url
+            'url': patch_url(chapter_index_url, chapter_url_node['href'])
         }
         chapter_urls.append(chapter_index_url)
 
@@ -203,16 +133,16 @@ def crawl_chapter_urls(chapter_index_url: str, selector_tag: str) -> list:
     return chapter_urls
 
 
-def crawl_chapter_content(chapter_url: str, selector_tag: str) -> str:
+def crawl_chapter_content(chapter_url: str, crawl_config: CrawlConfig) -> str:
     """
-    获取章节内容
+    爬取某一章节的内容
     :param chapter_url: 章节内容url
-    :param selector_tag: 选择器
+    :param crawl_config: 爬取配置
     :return: 章节内容
     """
-    chapter_content = u_file.get_content_with_cache(chapter_url, **_REQUESTS_KWARGS)
+    chapter_content = u_file.get_content_with_cache(chapter_url, **REQUESTS_KWARGS)
     chapter_soup = BeautifulSoup(chapter_content, 'lxml')
-    chapter_content_p_nodes = chapter_soup.select(SELECTOR_MAP[selector_tag]['chapter_content'])
+    chapter_content_p_nodes = chapter_soup.select(crawl_config.chapter_content_selector)
     if not chapter_content_p_nodes:
         log.error('get chapter content empty. chapter_url: {}'.format(chapter_url))
         return ''
@@ -222,28 +152,23 @@ def crawl_chapter_content(chapter_url: str, selector_tag: str) -> str:
     return chapter_content
 
 
-def crawl_content(chapter_index_url: str, novel_title: str):
+def crawl_novel_by_menu(chapter_index_url: str, novel_title: str):
     """
-    爬取小说内容
+    从小说章节目录主页爬取小说，首先获取所有的章节，然后依次爬取每个章节
     :param chapter_index_url:  章节目录页面地址
     :param novel_title: 小说标题
     :return:
     """
-    selector_tag_regex = re.compile('//([^/]+)/')
-    selector_tag_result = selector_tag_regex.search(chapter_index_url)
-    if not selector_tag_result or not selector_tag_result.groups():
-        log.error('can not match selector tag: {}'.format(chapter_index_url))
-        return False
-    selector_tag = selector_tag_result.groups()[0]
-    log.info('match selector tag: {}'.format(selector_tag))
-    chapter_urls = crawl_chapter_urls(chapter_index_url, selector_tag)
+    crawl_config = get_crawl_config(chapter_index_url)
+    log.info('crawl config')
+    chapter_urls = crawl_chapter_urls(chapter_index_url, crawl_config)
     novel_content = ''
     novel_save_path = f'result\\{novel_title}.txt'
 
     for chapter_url in chapter_urls:
         log.info('----> begin crawl chapter: {}'.format(chapter_url))
         novel_content += '\n\n' + chapter_url['title'] + '\n\n'
-        chapter_content = crawl_chapter_content(chapter_url['url'], selector_tag)
+        chapter_content = crawl_chapter_content(chapter_url['url'], crawl_config)
         if not chapter_content or len(chapter_content) <= 100:
             log.error('chapter content is less. chapter_url: {}, content: {}'.format(chapter_url, chapter_content))
             continue
@@ -251,97 +176,49 @@ def crawl_content(chapter_index_url: str, novel_title: str):
         u_file.write_content(novel_save_path, novel_content)
 
 
-def crawl_novel_list():
-    book_info_cache_file = r'result\yuri_book_infos.json'
-    book_infos = get_novel_list_from_bb(book_info_cache_file)
-    for book_info in book_infos:
-        log.info('patch book info: {}'.format(book_info['bookName']))
-        patch_novel_info(book_info)
-        u_file.dump_json_to_file(book_info_cache_file, book_infos)
-    book_infos = sorted(book_infos, key=lambda x: int(x['collectCount']), reverse=True)
-    u_file.dump_json_to_file(book_info_cache_file, book_infos)
-
-
-"""
-// Fiddler自定义抓包导出脚本，使用 boluobao-4.8.22.apk，逍遥模拟器，postern
-// 自动保存session到本地，需将该部分内容加到 OnBeforeResponse 中
-if (oSession.fullUrl.Contains("api.sfacg.com"))
-{  
-  var mathRegexes = {
-    'novel': /\/novels\/(\d+)\?/,
-    'chap': /\/Chaps\/(\d+)\?/,
-    'dir': /\/novels\/(\d+)\/dirs\?/
-  };
-
-  var savePath = "D:/work/other/";
-  for (var key in mathRegexes) {
-    // 匹配并保存请求
-    var matchResult = mathRegexes[key].exec(oSession.fullUrl);
-      if (matchResult && matchResult.length >= 2) {
-        oSession.SaveSession(savePath + key + '-' + matchResult[1] + '.txt', false);
-      }
-  }
-}
-"""
-
-
-def process_sf():
+def crawl_novel_by_next(begin_chapter_url: str, novel_title: str):
     """
-    处理sf抓包导出的小说内容转存，fiddler导出脚本如上
-    :return:
+    通过不断爬取下一章来爬取小说内容，从第一章开始直到最后一章
+    :param begin_chapter_url: 小说第一章内容
+    :param novel_title: 小说标题
+    :return: 小说完整内容
     """
-    save_path = r'D:\work\other\nn'
-    request_files = u_file.get_all_sub_files(save_path, contain_dir=False)
+    novel_save_path = f'result\\{novel_title}.txt'
 
-    def read_response_json(file_path: str) -> dict:
-        file_content = u_file.read_content(file_path)
-        split_token = 'X-Powered-By: ASP.NET'
-        split_index = file_content.find(split_token)
-        if split_index == -1:
-            log.error('The file response is not format.')
-            return {}
-        return json.loads(file_content[split_index + len(split_token):].strip())
+    novel_content = ''
+    next_chapter_url = begin_chapter_url
+    crawl_config = get_crawl_config(begin_chapter_url)
+    while next_chapter_url:
+        log.info('begin crawl url: {}'.format(next_chapter_url))
+        html_content = u_file.get_content_with_cache(next_chapter_url, **REQUESTS_KWARGS)
 
-    novel_info_map = {}
-    dir_info_map = {}
-    chap_content_map = {}
+        if not html_content:
+            log.error('crawl error for url: {}'.format(next_chapter_url))
+            continue
 
-    for request_file in request_files:
-        if 'novel' in request_file:
-            novel_info = read_response_json(request_file)
-            novel_info = novel_info['data']
-            novel_info_map[novel_info['novelId']] = novel_info
-        if 'dir' in request_file:
-            dir_info = read_response_json(request_file)
-            dir_info = dir_info['data']
-            dir_info_map[dir_info['novelId']] = dir_info
-        if 'chap' in request_file:
-            chap_content = read_response_json(request_file)
-            chap_content = chap_content['data']
-            chap_content_map[chap_content['chapId']] = chap_content
+        chapter_soup = BeautifulSoup(html_content, 'lxml')
 
-    for novel_info in novel_info_map.values():
-        novel_id = novel_info['novelId']
-        dir_info = dir_info_map[novel_id]
-        novel_content = ''
-        log.info('begin process novel. id: {}, name: {}'.format(novel_id, novel_info['novelName']))
-        for volume in dir_info['volumeList']:
-            novel_content += '\n\n{}\n\n'.format(volume['title'])
-            for chapter in volume['chapterList']:
-                log.info('process novel chapter, novel_id: {}, chapter_id: {}'.format(novel_id, chapter['chapId']))
-                novel_content += '\n\n{}\n'.format(chapter['title'])
-                chapter_id = chapter['chapId']
-                if chapter_id not in chap_content_map:
-                    log.warn('The chapter is not download: {}'.format(chapter_id))
-                    continue
-                novel_content += chap_content_map[chapter_id]['expand']['content']
+        # 获取下一章的url
+        next_chapter_url_node = chapter_soup.select_one(crawl_config.next_chapter_url_selector)
+        if next_chapter_url_node:
+            next_chapter_url = patch_url(begin_chapter_url, next_chapter_url_node['href'])
 
-            u_file.write_content(r'result/{}.txt'.format(novel_info['novelName']), novel_content)
+        # 章节标题
+        chapter_title_node = chapter_soup.select_one(crawl_config.chapter_title_selector)
+        novel_content += '\n\n' + chapter_title_node.text + '\n'
+
+        # 章节内容
+        chapter_content_node = chapter_soup.select_one(crawl_config.chapter_content_selector)
+        chapter_content = chapter_content_node.text
+        if len(chapter_content) <= 500:
+            log.error('content is less: {}'.format(next_chapter_url))
+            continue
+        novel_content += chapter_content
+
+        u_file.write_content(novel_save_path, novel_content)
+    return novel_content
 
 
 if __name__ == '__main__':
-    # book_infos = get_novel_list_from_bb()
-    process_sf()
-    # info = patch_novel_info(book_infos[0])
-    # crawl_novel_list()
-    # crawl_content('https://book.sfacg.com/Novel/552847/MainIndex/', '病娇徒儿对天生媚骨的我图谋不轨')
+    crawl_novel_by_next('https://ncode.syosetu.com/n6316bn/1/', '転生したらスライムだった件')
+    # crawl_novel_by_next('https://325def1d9a6.12bqg.com/book/152211/1.html', '转生冰山大小姐也不要被她们贴')
