68016231a51c6cee0ce6cfaa3b289ca0896268f2
uusama <1406558940@qq.com>
2024-10-18 01:06:02 +0800

    init note.

---
+++ tool/stradew_valley_mod_manager.py
@@ -0,0 +1,171 @@
+"""
+星露谷物语游戏的mod管理器
+"""
+
+MOD_INFOS = [
+    {
+        'id': 10341,
+        'name': '姜岛别墅',
+        'type': '地图',
+        'tag': [''],
+        'description': '',
+    },
+    {
+        'id': 10510,
+        'name': '盐崖农场',
+        'type': '地图',
+        'tag': [''],
+        'description': '',
+    }
+]
+
+参考：  https://www.bilibili.com/read/cv12131406/
+
+mod作者推荐
+loli:
+
+地图
+蓝草 10635
+小香风稻草人 11669
+花花房子 11579
+NPC装饰 11581
+棉花糖栅栏 11622
+避雷针 11634
+猫头鹰稻草人 11679
+晶石传送塔 11312
+史莱姆屋 11418
+海洋糖果屋建筑 11890
+三丽欧咖啡厅 10546
+咖啡车 11921
+糖果屋女巫小屋 10493
+紫色月亮建筑 10701
+蔷薇少女稻草人 10585
+鸡舍 11032
+花花洒水器 10912
+玫瑰建筑 10656
+地板 11265
+花店 11732
+传送塔 11887
+陨石美化 11245
+温室 11101
+AT模式多颜色树 10719
+栅栏 10528
+玻璃帐篷 10983
+更好的山顶农场 10788
+草甸农场 10790
+草莓小屋 9857
+粉色家具 9698
+电视机节目美化 9387
+
+
+
+家具
+月亮星球灯 11696
+月亮星球路灯 11159
+浴室家具 10982
+咖啡厅家具 10274
+梦幻厨房 11875
+拓展窗户 11807
+壁纸地板（粉色） 11184
+壁纸地板（浅色） 11896
+壁纸地板 10840
+招财猫雕像 11531
+粉色家具 11509
+壁纸 11460
+壁纸和地板 11525
+白色厨房 11431
+优雅工艺品 11693
+
+界面美化 6166 4697 8142 关键词：interface
+人物 7331 6134 关键词：shirts hairstyles
+地图 4705 4369 关键词：recolor recolour
+栅栏等 4870 4852 5407 7708 7754
+房屋 4542 4564 关键词：buildings
+工具路灯等 4613 4785 5301 5513
+房屋内部 6005 5857 关键词：wallpaper floor
+家具 1024 5351 5444 6531 关键词：furniture
+动物 2964 6102 8088 4618
+功能 7001 5849
+
+星露谷拓展：https://www.nexusmods.com/stardewvalley/mods/3753
+爷爷的农场：https://www.nexusmods.com/stardewvalley/mods/3753
+界面美化：https://www.nexusmods.com/stardewvalley/mods/13658
+地图颜色和城镇：https://www.nexusmods.com/stardewvalley/mods/4705
+房屋外部美化：https://yellog.postype.com/post/11565362
+房屋内部美化：https://www.nexusmods.com/stardewvalley/mods/13752
+猫猫变小熊猫：https://www.nexusmods.com/stardewvalley/mods/4409
+
+1 UI信息：https://github.com/Annosz/UIInfoSuite2/releases
+2 查找一切：https://www.nexusmods.com/stardewvalley/mods/541
+3 梯子定位：https://www.nexusmods.com/stardewvalley/mods/3094
+4 镰刀收割：https://www.nexusmods.com/stardewvalley/mods/2731
+5 快速动画：https://www.nexusmods.com/stardewvalley/mods/1089
+6 NPC位置：https://www.nexusmods.com/stardewvalley/mods/239
+7 小马减肥计划：https://www.nexusmods.com/stardewvalley/mods/1232
+8 可见鱼：https://www.nexusmods.com/stardewvalley/mods/8897
+9 无限视野：https://www.nexusmods.com/stardewvalley/mods/7363
+
+1 地图美化：https://www.nexusmods.com/stardewvalley/mods/5255
+2 房屋美化：https://www.nexusmods.com/stardewvalley/mods/2110
+3 家具美化：https://www.nexusmods.com/stardewvalley/mods/11684
+
+加了两个功能mod：
+1 NPC好感度不降低：https://www.nexusmods.com/stardewvalley/mods/1738
+2 NPC触发事件查询：https://www.nexusmods.com/stardewvalley/mods/8505
+
+
+人物
+蝴蝶面纱 11635
+发型合集 11724
+帽子合集 11780
+鞋子美化 11203
+新年衬衫 10523
+蝴蝶帽子 10699
+兔子拖鞋 10684
+头发 11909
+衣服 9859
+
+其他
+优雅工具 11479
+棉花糖祝尼魔跟随 11385
+婚纱商人 11446
+爱情作物 11477
+上古水果美化 11505
+古物美化 11862
+垃圾美化 11052
+糖果食物 10737
+垃圾美化 11049
+樱花界面 11266
+添加柿子 11770
+工具带花 6764
+
+动物
+灰尘精灵 11219
+猫猫 11487
+小鸡美化 11870
+策马奔跑 11879
+
+
+NPC
+行走头图像 11298
+Q版头像（全套） 11491
+Q版头像 10442
+所有NPC头像 11654
+女性NPC行走图像 10990
+男性NPC行走图像 11797
+
+功能
+立刻完成成就 11515
+查找一切 541
+更好的牧场 859
+小马减肥计划 1232
+镜子和衣柜 4734
+
+可能影响游戏平衡的mod
+梯子定位 3094
+战斗机器人 4507 （不兼容）
+沙漠矿洞电梯 963
+跳过钓鱼动画 2697
+拖拉机 1401
+扳手 93
+作弊菜单 4
